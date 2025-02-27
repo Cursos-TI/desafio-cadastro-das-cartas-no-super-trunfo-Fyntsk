1ac895e8cff109c80a6f639dc51c09b9fd580b92
Fyntsk <jean.caasemiro@gmail.com>
2025-02-27 14:22:43 +0000

    ATT0.1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
     int main() {
+    char estado;
+    char codigocarta[20], codigocidade[30];
+    int populacao;
+    float areakm, pib;
+    int pontosturisticos;
+
+    
+    
      
 
     return 0;
