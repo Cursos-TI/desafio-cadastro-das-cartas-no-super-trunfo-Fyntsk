7f2f4d7cea892faf8c751ebfee1eecfe86c2ac2c
Fyntsk <jean.caasemiro@gmail.com>
2025-02-27 17:17:22 +0000

    att2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
-    int main() {
-    char estado;
-    char codigocarta[20], codigocidade[30];
-    int populacao;
-    float areakm, pib;
-    int pontosturisticos;
+    int main(){    
+        char estado[20], cidade[22];
+        float populacao;
+    
+
+        printf("Digite seu estado:");
+        scanf("%s", estado);
+        
+        printf("Digite sua cidade:");
+        scanf("%s", cidade);
+
+        printf("Digite a população:");
+        scanf("%f", &populacao);
+
+        printf("Nome do Estado: %s\n - Nome da Cidade: %s\n", estado, cidade);
+        printf("População: %f Milhões", populacao);
+        
+
 
     
-    
-     
 
-    return 0;
+        
+    
+    
+    
 }
