1e2b584dd8f8a87cb008dba29ad42fcd1227042c
Fyntsk <jean.caasemiro@gmail.com>
2025-02-28 11:43:26 +0000

    att5

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,8 @@
 
     int main(){    
         char estado[20], cidade[22];
-        int populacao;
+        int populacao, pontosturisticos;
+        float area, pib;
     
 
         printf("Digite seu estado:");
@@ -14,11 +15,22 @@
         printf("Digite a população:");
         scanf("%d", &populacao);
 
+        printf("Digite a área em M²:");
+        scanf("%f", &area);
+
+        printf("Digite o PIB:");
+        scanf("%f", &pib);
+
+        printf("Digite o número de pontos turísticos:");
+        scanf("%d", &pontosturisticos);
+
         printf("Carta 01\n");
         printf("Nome do Estado: %s\n", estado);
         printf("Nome da cidade: %s\n", cidade);
-        printf("População: %d habitantes", populacao);
-        
+        printf("População: %d habitantes\n", populacao);
+        printf("Área de: %.3f M²\n", area);
+        printf("PIB de: %.2f bilhões de reais\n", pib);
+        printf("Pontos turísticos: %d\n", pontosturisticos);
 
 
     
