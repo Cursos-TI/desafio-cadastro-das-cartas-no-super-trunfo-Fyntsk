2bd1ea8b825cbdcc6af60f05834e21e25c7a2114
Fyntsk <jean.caasemiro@gmail.com>
2025-03-20 11:23:31 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -123,6 +123,7 @@
         scanf("%d", &opcao);
 
         int resultado, resultado2;
+        float somaCarta1 = 0, somaCarta2 = 0;
 
         switch (opcao){           //IMPLEMENTAÇÃO DO SWITCH     
         case 1:
@@ -130,7 +131,10 @@
                printf("%s: %lu habitantes.\n", estado, populacao);
                printf("%s: %lu habitantes.\n", estado2, populacao2);
 
-             if  (resultado = (populacao > populacao2) ? 1 : (populacao < populacao2) ? 0 : -1);
+             resultado = (populacao > populacao2) ? 1 : (populacao < populacao2) ? 0 : -1;
+             
+             somaCarta1 += populacao; //Adiciona os valores para realizar a soma final dos atributos
+             somaCarta2 += populacao2;
                
         break;
         case 2:
@@ -138,15 +142,21 @@
                printf("%s: %.3f M².\n", estado, area);
                printf("%s: %.3f M².\n", estado2, area2);
 
-             if (resultado = (area > area2) ? 1 : (area < area2) ? 0 : -1);
+             resultado = (area > area2) ? 1 : (area < area2) ? 0 : -1;
+
+             somaCarta1 += area;
+             somaCarta2 += area2;
               
         break;
         case 3:
                printf("Você escolheu: PIB.\n");
                printf("%s: %.2f bilhões de reais.\n", estado, pib);
                printf("%s: %.2f bilhões de reais.\n", estado2, pib2);
+ 
+             resultado = (pib > pib2) ? 1 : (pib < pib2) ? 0 : -1;
 
-             if (resultado = (pib > pib2) ? 1 : (pib < pib2) ? 0 : -1);
+             somaCarta1 += pib;
+             somaCarta2 += pib2;
                 
         break;
         case 4:
@@ -154,7 +164,10 @@
                printf("%s: %d pontos turísticos.\n", estado, pontosturisticos);
                printf("%s: %d pontos turísticos.\n", estado2, pontosturisticos2);
 
-             if (resultado = (pontosturisticos > pontosturisticos2) ? 1 : (pontosturisticos < pontosturisticos2) ? 0 : -1);
+             resultado = (pontosturisticos > pontosturisticos2) ? 1 : (pontosturisticos < pontosturisticos2) ? 0 : -1;
+
+               somaCarta1 += pontosturisticos;
+               somaCarta2 += pontosturisticos2;
                 
         break;
         case 5:
@@ -162,7 +175,10 @@
                printf("%s: %.2f hab/km².\n", estado, densidade);
                printf("%s: %.2f hab/km².\n", estado2, densidade2);
 
-             if (resultado = (densidade < densidade2) ? 1 : (densidade > densidade2) ? 0 : -1);
+             resultado = (densidade < densidade2) ? 1 : (densidade > densidade2) ? 0 : -1;
+
+               somaCarta1 += densidade;
+               somaCarta2 += densidade2;
                
 
         break;
@@ -171,13 +187,13 @@
         }      
 
         if (resultado == 1){
-              printf("CARTA 01 - %s VENCEU!\n", estado);
+              printf("CARTA 01 - %s VENCEU!\n\n", estado);
   
               } else if (resultado == 0) {
-                printf("CARTA 02 - %s VENCEU!\n",estado2);
+                printf("CARTA 02 - %s VENCEU!\n\n",estado2);
   
               } else {
-                printf("Atributo empatado!\n");  
+                printf("Atributo empatado!\n\n");  
               } 
        
         printf("Selecione um segundo atributo para comparação:\n");
@@ -199,7 +215,10 @@
               printf("%s: %lu habitantes.\n", estado, populacao);
               printf("%s: %lu habitantes.\n", estado2, populacao2);
 
-            if (resultado2 = (populacao > populacao2) ? 1 : (populacao < populacao2) ? 0 : -1);
+            resultado2 = (populacao > populacao2) ? 1 : (populacao < populacao2) ? 0 : -1;
+
+            somaCarta1 += populacao;
+            somaCarta2 += populacao2;
               
        break;
        case 2:
@@ -207,7 +226,10 @@
               printf("%s: %.3f M².\n", estado, area);
               printf("%s: %.3f M².\n", estado2, area2);
 
-            if (resultado2 = (area > area2) ? 1 : (area < area2) ? 0 : -1);
+            resultado2 = (area > area2) ? 1 : (area < area2) ? 0 : -1;
+
+            somaCarta1 += area;
+            somaCarta2 += area2;
              
        break;
        case 3:
@@ -215,7 +237,10 @@
               printf("%s: %.2f bilhões de reais.\n", estado, pib);
               printf("%s: %.2f bilhões de reais.\n", estado2, pib2);
 
-            if (resultado2 = (pib > pib2) ? 1 : (pib < pib2) ? 0 : -1);
+            resultado2 = (pib > pib2) ? 1 : (pib < pib2) ? 0 : -1;
+
+            somaCarta1 += pib;
+            somaCarta2 += pib2;
                
        break;
        case 4:
@@ -223,7 +248,10 @@
               printf("%s: %d pontos turísticos.\n", estado, pontosturisticos);
               printf("%s: %d pontos turísticos.\n", estado2, pontosturisticos2);
 
-            if (resultado2 = (pontosturisticos > pontosturisticos2) ? 1 : (pontosturisticos < pontosturisticos2) ? 0 : -1);
+            resultado2 = (pontosturisticos > pontosturisticos2) ? 1 : (pontosturisticos < pontosturisticos2) ? 0 : -1;
+
+            somaCarta1 += pontosturisticos;
+            somaCarta2 += pontosturisticos2;
                
        break;
        case 5:
@@ -231,7 +259,10 @@
               printf("%s: %.2f hab/km².\n", estado, densidade);
               printf("%s: %.2f hab/km².\n", estado2, densidade2);
 
-            if (resultado2 = (densidade < densidade2) ? 1 : (densidade > densidade2) ? 0 : -1);
+            resultado2 = (densidade < densidade2) ? 1 : (densidade > densidade2) ? 0 : -1;
+
+            somaCarta1 += densidade;
+            somaCarta2 += densidade2;
               
 
        break;
@@ -249,8 +280,22 @@
             } else {
               printf("Atributo empatado!\n");  
             } 
+       }
+     
 
-        return 0;
-   
+      printf("\n** RESULTADO FINAL DA RODADA **\n");
+      printf("Soma dos atributos escolhidos para %s: %.2f\n", estado, somaCarta1);
+      printf("Soma dos atributos escolhidos para %s: %.2f\n", estado2, somaCarta2);
+
+      if (somaCarta1 > somaCarta2) {
+          printf("CARTA 01 - %s VENCEU A RODADA!\n", estado);
+      } else if (somaCarta1 < somaCarta2) {
+          printf("CARTA 02 - %s VENCEU A RODADA!\n", estado2);
+      } else {
+          printf("A rodada terminou em EMPATE!\n");
 }
+           
+  
 }
+
+
