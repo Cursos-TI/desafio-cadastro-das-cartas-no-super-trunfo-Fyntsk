37835c6c300d25acdbbc1b040141e849d9b0fdef
Jean Casemiro Marçal <jean.caasemiro@gmail.com>
2025-04-24 16:28:47 +0000

    CARTAS SF FINALIZADO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+ 
 
     int main(){    //VARIAVEIS//
         char estado[20], cidade[22], codigo[10], estado2[30], cidade2[40], codigo2[15];
@@ -108,194 +108,16 @@
         printf("PIB per capita: R$ %.2f reais por habitante.\n", pibpercapita2);
         printf("Super Poder: %.2f\n\n", superPoder2);
 
-        int opcao, opcao2;
+        printf("\n====== RESULTADOS DAS COMPARAÇÕES ======\n");
 
-        //INSERÇÃO DO MENU PARA QUE O ÚSUARIO SELECIONE O ATRIBUTO ESCOLHIDO
+        printf("População: %d - %s\n", populacao > populacao2, populacao > populacao2 ? "Carta 1 venceu" : "Carta 2 venceu");
+        printf("Área: %d - %s\n", area > area2, area > area2 ? "Carta 1 venceu" : "Carta 2 venceu");
+        printf("PIB: %d - %s\n", pib > pib2, pib > pib2 ? "Carta 1 venceu" : "Carta 2 venceu");
+        printf("Pontos turísticos: %d - %s\n", pontosturisticos > pontosturisticos2, pontosturisticos > pontosturisticos2 ? "Carta 1 venceu" : "Carta 2 venceu");
+        printf("Densidade populacional (menor vence): %d - %s\n", densidade < densidade2, densidade < densidade2 ? "Carta 1 venceu" : "Carta 2 venceu");
+        printf("PIB per capita: %d - %s\n", pibpercapita > pibpercapita2, pibpercapita > pibpercapita2 ? "Carta 1 venceu" : "Carta 2 venceu");
+        printf("Super Poder: %d - %s\n", superPoder1 > superPoder2, superPoder1 > superPoder2 ? "Carta 1 venceu" : "Carta 2 venceu");
 
-        printf("** MENU PARA SELEÇÃO DE ATRIBUTOS PARA COMPARAÇÃO**\n");
-        printf("Selecione um atributo para comparação:\n");
-        printf("1. População.\n");
-        printf("2. Área.\n");
-        printf("3. PIB.\n");
-        printf("4. Pontos turísticos.\n");
-        printf("5. Densidade Populacional.\n");
-        printf("Digite a primeira opção escolhida:");
-        scanf("%d", &opcao);
+      return 0;
 
-        int resultado, resultado2;
-        float somaCarta1 = 0, somaCarta2 = 0;
-
-        switch (opcao){           //IMPLEMENTAÇÃO DO SWITCH     
-        case 1:
-               printf("Você escolheu: População.\n");
-               printf("%s: %lu habitantes.\n", estado, populacao);
-               printf("%s: %lu habitantes.\n", estado2, populacao2);
-
-             resultado = (populacao > populacao2) ? 1 : (populacao < populacao2) ? 0 : -1;
-             
-             somaCarta1 += populacao; //Adiciona os valores para realizar a soma final dos atributos
-             somaCarta2 += populacao2;
-               
-        break;
-        case 2:
-               printf("Você escolheu: Área.\n");
-               printf("%s: %.3f M².\n", estado, area);
-               printf("%s: %.3f M².\n", estado2, area2);
-
-             resultado = (area > area2) ? 1 : (area < area2) ? 0 : -1;
-
-             somaCarta1 += area;
-             somaCarta2 += area2;
-              
-        break;
-        case 3:
-               printf("Você escolheu: PIB.\n");
-               printf("%s: %.2f bilhões de reais.\n", estado, pib);
-               printf("%s: %.2f bilhões de reais.\n", estado2, pib2);
- 
-             resultado = (pib > pib2) ? 1 : (pib < pib2) ? 0 : -1;
-
-             somaCarta1 += pib;
-             somaCarta2 += pib2;
-                
-        break;
-        case 4:
-               printf("Você escolheu: Pontos turísticos.\n");
-               printf("%s: %d pontos turísticos.\n", estado, pontosturisticos);
-               printf("%s: %d pontos turísticos.\n", estado2, pontosturisticos2);
-
-             resultado = (pontosturisticos > pontosturisticos2) ? 1 : (pontosturisticos < pontosturisticos2) ? 0 : -1;
-
-               somaCarta1 += pontosturisticos;
-               somaCarta2 += pontosturisticos2;
-                
-        break;
-        case 5:
-               printf("Você escolheu: Densidade populacional.\n");
-               printf("%s: %.2f hab/km².\n", estado, densidade);
-               printf("%s: %.2f hab/km².\n", estado2, densidade2);
-
-             resultado = (densidade < densidade2) ? 1 : (densidade > densidade2) ? 0 : -1;
-
-               somaCarta1 += densidade;
-               somaCarta2 += densidade2;
-               
-
-        break;
-        default:
-               printf("Opção inválida.\n");
-        }      
-
-        if (resultado == 1){
-              printf("CARTA 01 - %s VENCEU!\n\n", estado);
-  
-              } else if (resultado == 0) {
-                printf("CARTA 02 - %s VENCEU!\n\n",estado2);
-  
-              } else {
-                printf("Atributo empatado!\n\n");  
-              } 
-       
-        printf("Selecione um segundo atributo para comparação:\n");
-        printf("1. População.\n");
-        printf("2. Área.\n");
-        printf("3. PIB.\n");
-        printf("4. Pontos turísticos.\n");
-        printf("5. Densidade Populacional.\n");
-        printf("Digite a segunda opção escolhida:");
-        scanf("%d", &opcao2);
-
-        if (opcao == opcao2){
-            printf("Os atributos escolhidos são iguais.\n");
-       
-       }   else {
-           switch (opcao2){
-              case 1:
-              printf("Você escolheu: População.\n");
-              printf("%s: %lu habitantes.\n", estado, populacao);
-              printf("%s: %lu habitantes.\n", estado2, populacao2);
-
-            resultado2 = (populacao > populacao2) ? 1 : (populacao < populacao2) ? 0 : -1;
-
-            somaCarta1 += populacao;
-            somaCarta2 += populacao2;
-              
-       break;
-       case 2:
-              printf("Você escolheu: Área.\n");
-              printf("%s: %.3f M².\n", estado, area);
-              printf("%s: %.3f M².\n", estado2, area2);
-
-            resultado2 = (area > area2) ? 1 : (area < area2) ? 0 : -1;
-
-            somaCarta1 += area;
-            somaCarta2 += area2;
-             
-       break;
-       case 3:
-              printf("Você escolheu: PIB.\n");
-              printf("%s: %.2f bilhões de reais.\n", estado, pib);
-              printf("%s: %.2f bilhões de reais.\n", estado2, pib2);
-
-            resultado2 = (pib > pib2) ? 1 : (pib < pib2) ? 0 : -1;
-
-            somaCarta1 += pib;
-            somaCarta2 += pib2;
-               
-       break;
-       case 4:
-              printf("Você escolheu: Pontos turísticos.\n");
-              printf("%s: %d pontos turísticos.\n", estado, pontosturisticos);
-              printf("%s: %d pontos turísticos.\n", estado2, pontosturisticos2);
-
-            resultado2 = (pontosturisticos > pontosturisticos2) ? 1 : (pontosturisticos < pontosturisticos2) ? 0 : -1;
-
-            somaCarta1 += pontosturisticos;
-            somaCarta2 += pontosturisticos2;
-               
-       break;
-       case 5:
-              printf("Você escolheu: Densidade populacional.\n");
-              printf("%s: %.2f hab/km².\n", estado, densidade);
-              printf("%s: %.2f hab/km².\n", estado2, densidade2);
-
-            resultado2 = (densidade < densidade2) ? 1 : (densidade > densidade2) ? 0 : -1;
-
-            somaCarta1 += densidade;
-            somaCarta2 += densidade2;
-              
-
-       break;
-       default:
-              printf("Opção inválida.\n");
-       }
-       
-       
-       if (resultado2 == 1){
-            printf("CARTA 01 - %s VENCEU!\n", estado);
-
-            } else if (resultado2 == 0) {
-              printf("CARTA 02 - %s VENCEU!\n",estado2);
-
-            } else {
-              printf("Atributo empatado!\n");  
-            } 
-       }
-     
-
-      printf("\n** RESULTADO FINAL DA RODADA **\n");
-      printf("Soma dos atributos escolhidos para %s: %.2f\n", estado, somaCarta1);
-      printf("Soma dos atributos escolhidos para %s: %.2f\n", estado2, somaCarta2);
-
-      if (somaCarta1 > somaCarta2) {
-          printf("CARTA 01 - %s VENCEU A RODADA!\n", estado);
-      } else if (somaCarta1 < somaCarta2) {
-          printf("CARTA 02 - %s VENCEU A RODADA!\n", estado2);
-      } else {
-          printf("A rodada terminou em EMPATE!\n");
-}
-           
-  
-}
-
-
+    }
